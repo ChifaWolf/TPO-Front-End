7a78ffbc7c3b76d088ec3c193e32a537eb713fca
ChifaWolf <86135683+ChifaWolf@users.noreply.github.com>
2021-10-20 14:47:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    let y = document.forms["myForm"]["fsurname"].value;
+    let x = document.forms["formulario"]["nombre"].value;
+    let y = document.forms["formulario"]["apellido"].value;
+    let z = document.forms["formulario"]["telefono"].value;
 
-    if (x == "" && y == "") {
-        alert("Completar Nombre y Apellido");
+    if (x == "" && y == "" && z == "") {
+        alert("Completar Nombre, Apellido y Telefono");
         return false;
       }
-    
-    
+
+      if (x == "" && y == "") {
+        alert("Completar Nombre y Aoellido.");
+        return false;
+      }
+
+      if (x == "" && z == "") {
+        alert("Completar Nombre y Telefono.");
+        return false;
+      }
+
+      if (y == "" && z == "") {
+        alert("Completar Apellido y Telefono.");
+        return false;
+      }
+
     if (x == "") {
       alert("Campo Nombre Incompleto");
       return false;
@@ -16,6 +31,11 @@ function validateForm() {
     
     if (y == "") {
       alert("Campo Apellido incompleto");
+      return false;
+    }
+
+    if (z == "") {
+      alert("Campo Telefono incompleto");
       return false;
     }
   }
